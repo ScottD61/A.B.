72e867d558e212001160613d18167d25ef3ccdbb
Scott Davis <ScDavis6@gmail.com>
2015-07-28 16:59:14 -0500

    Fraud project

--- BoaAir.R
+++ BoaAir.R
@@ -5,22 +5,30 @@ fraud <- read.csv("/Users/scdavis6/Fraud/BoaAir.csv", sep = ",", na.strings = ""
 summary(fraud)
 str(fraud)
 #Convert to time series objects
-#Convert date column from vector to date object
-#yes
+#Convert date column from factor to date object
 fraud$FIN.TRANSACTION.DATE <- as.Date(fraud$FIN.TRANSACTION.DATE, "%m/%d/%y")
+#Convert numeric and date to xts objects
+library(xts)
+fraud.xts <- xts(fraud$FIN.TRANSACTION.AMOUNT, fraud$FIN.TRANSACTION.DATE)
 
-
-#check output
-head(fraud$FIN.TRANSACTION.DATE, 5)
 
 #Visualizations
 #Histogram
 hist(fraud$FIN.TRANSACTION.AMOUNT)
 #Boxplot
-boxplot(fraud$FIN.TRANSACTION.AMOUNT)
+boxplot(fraud$FIN.TRANSACTION.AMOUNT, range = 500)
+#Toooo many outliers
 boxplot.stats(fraud$FIN.TRANSACTION.AMOUNT)$out
-#result has over 3.4k outliers
+#Time series plot - don't use
+plot(fraud$FIN.TRANSACTION.DATE, fraud$FIN.TRANSACTION.AMOUNT, ylab = "Transaction in $", 
+     xlab = "Time in Months")
+#Fine for now, but fix later
+plot(fraud.xts, type = "l", ylab = "Transaction in $", xlab = "Time in Months")
 
+hist(fraud.xts)
+
+library(DMwR)
+outliers <- lofactor(fraud$FIN.TRANSACTION.AMOUNT)
 
 #To do:
 #1) see how to use stl() function and converting finan.trans to ts object
